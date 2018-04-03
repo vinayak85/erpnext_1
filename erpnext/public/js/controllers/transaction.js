40ff02316f6bab1be10ac9486adf824e94c0f863
vinayak85 <vinupatil9@gmail.com>
2018-04-03 16:50:32 +0530

    Update transaction.js

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -6,6 +6,7 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 		this._super();		
 		var df = frappe.meta.get_docfield(this.frm.doctype + " Item","item_code", cur_frm.doc.name);		
                	df.hidden = 1;
+		alert(this.frm.is_return);
 		//frm.toggle_display(df, false);
 		//objToString(df);
 		frappe.ui.form.on(this.frm.doctype + " Item", "rate", function(frm, cdt, cdn) {
