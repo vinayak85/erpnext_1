034f05bdbf7af0cd5cca0f73bb6169f090d88044
vinayak85 <vinupatil9@gmail.com>
2018-04-06 17:02:12 +0530

    Update transaction.js

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -236,12 +236,12 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 		if(!this.frm.doc.is_return) {
 			var df = frappe.meta.get_docfield(this.frm.doctype + " Item","against_invoice", this.frm.doc.name);		
                         df.hidden = 1;
-			//alert("No Ret");
+	                 alert("No Ret");
 			//df.read_only = 1;
 		}else{
 			var df = frappe.meta.get_docfield(this.frm.doctype + " Item","against_invoice", this.frm.doc.name);		
                         df.hidden = 0;
-			//alert(" Ret");
+			alert(" Ret");
 			//df.read_only = 0;
 		}
 			
