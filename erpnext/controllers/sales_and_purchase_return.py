a7bc99328ece79e38827d1f5f99cc2f1a1598c32
vinayak85 <vinupatil9@gmail.com>
2018-04-21 17:07:12 +0530

    Update sales_and_purchase_return.py

--- erpnext/controllers/sales_and_purchase_return.py
+++ erpnext/controllers/sales_and_purchase_return.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.utils import flt, get_datetime, format_datetime
-from msgprint
+from frappe import msgprint, _
 
 class StockOverReturnError(frappe.ValidationError): pass
 
