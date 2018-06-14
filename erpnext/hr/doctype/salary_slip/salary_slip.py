a3b253a7e62bc9cebb393d093da4ef565bd7f72e
vinayak85 <vinupatil9@gmail.com>
2018-06-14 13:28:47 +0530

    Update salary_slip.py

--- erpnext/hr/doctype/salary_slip/salary_slip.py
+++ erpnext/hr/doctype/salary_slip/salary_slip.py
@@ -228,6 +228,9 @@ class SalarySlip(TransactionBase):
 
 		holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
 		working_days = date_diff(self.end_date, self.start_date) + 1
+		#vin code start
+		frappe.msgprint(_(self.end_date));
+		#vin code end
 		if not cint(frappe.db.get_value("HR Settings", None, "include_holidays_in_total_working_days")):
 			working_days -= len(holidays)
 			if working_days < 0:
