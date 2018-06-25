ae315868621a660b763be2e2b94c2f4ebcc39497
vinayak85 <vinupatil9@gmail.com>
2018-06-25 17:14:34 +0530

    Update queries.py

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -6,6 +6,7 @@ import frappe
 from frappe.desk.reportview import get_match_cond, get_filters_cond
 from frappe.utils import nowdate
 from collections import defaultdict
+from frappe import msgprint, _
 
 
  # searches for active employees
@@ -239,6 +240,7 @@ def get_delivery_notes_to_be_billed(doctype, txt, searchfield, start, page_len, 
 	}, { "txt": ("%%%s%%" % txt) }, as_dict=as_dict)
 
 def get_batch_no(doctype, txt, searchfield, start, page_len, filters):
+	msgprint(_(doctype));
 	cond = ""
 	if filters.get("posting_date"):
 		cond = "and (ifnull(batch.expiry_date, '')='' or batch.expiry_date >= %(posting_date)s)"
