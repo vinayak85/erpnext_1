6e634bbfa4c1547be5bb25b8be7ab2ada0954b8f
vinayak85 <vinupatil9@gmail.com>
2018-09-21 11:18:31 +0530

    Update sales_invoice.js

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -44,9 +44,8 @@ erpnext.accounts.SalesInvoiceController = erpnext.selling.SellingController.exte
 		
 		//arjun code start
 		if(doc.is_return) {
-			cur_frm.fields_dict.items.grid.fields_map.against_invoice_1.hidden = 1;
-			cur_frm.refresh_field(“items”);
-			
+			//cur_frm.fields_dict.items.grid.fields_map.against_invoice_1.hidden = 1;
+			//cur_frm.refresh_field(“items”);			
 			alert("return");
 		}
 		//arjun code end
